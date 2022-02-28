2cb6f9d190bab45adc5fd0abe1ba27c068a96de8
= <1107152488@qq.com>
2022-02-28 16:06:12 +0800

    feat(helloworld router/index.ts): add helloworld component counter's function and router
    add helloworld component counter's function and router's options

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,18 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router"
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/helloWorld",
+    component: () => import("../views/HelloWorld.vue"),
+    meta: {
+      title: "问候页",
+    },
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>计数器的功能：{{ counter }}</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const counter = ref<number>(0);
+const addition = () => {
+  counter.value++;
+};
+</script>
+
+<style scoped></style>
